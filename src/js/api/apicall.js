bd1f51b05f9b0ad7286f96d9729e34201a8c352c
James Bubb <jpbubb82@gmail.com>
2018-04-10 09:46:24 +0100

    Add APICall function

---
+++ src/js/api/apicall.js
@@ -0,0 +1,41 @@
+import { resolve } from "url";
+
+export default function apiCall(route, body = {}, method = 'GET') {
+    const timeoutDuration = 5000;
+    const request = new Promise((resolve, reject) => {
+        const headers = new Headers({
+            'Content-Type': 'application/json',
+        });
+
+        const requestDetails = {
+            method,
+            mode: 'cors',
+            headers,
+        };
+
+        function handleErrors(response) {
+            if (response.ok) {
+                return response.json();
+            } else {
+                throw Error(response.statusText);
+            }
+        }
+
+        fetch(`${SERVER_URL}/${route}`, requestDetails)
+            .then(handleErrors)
+            .then(resolve)
+            .catch(reject);
+
+    });
+
+    const timeout = new Promise((request, reject) => {
+        setTimeout(reject, timeoutDuration, 'Request Timed out!');
+    });
+
+    return new Promise((resolve, reject) => {
+        Promise.race([request, timeout])
+            .then(resolve)
+            .catch(reject);
+    });
+
+}
